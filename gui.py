b274e9cbd595c52c3f7d22246cce23e7fcbb6c62
Jamason Davis <jpdavis2471@gmail.com>
2025-04-30 15:17:23 -0500

    "Finished" UI.

--- gui.py
+++ gui.py
@@ -4,32 +4,36 @@ import tkinter as tk
 def placeElements(root):
     # Application Label.
     name = tk.Label(root, text="Creature Creator")
-    name.pack(side="top", anchor="w", padx=5, pady=5)
+    name.grid(row=0, column=0, columnspan=3, sticky="w", padx=5, pady=3)
 
     # Create the first frame for the first creature.
     creatureOneFrame = tk.Frame(root)
-    creatureOneFrame.pack(side="top", anchor="w", padx=30, pady=10)
+    creatureOneFrame.grid(row=1, column=0, sticky="w", padx=10, pady=15)
     # Populate the first frame with a label and an entry.
     label1 = tk.Label(creatureOneFrame, text="Creature 1:")
-    label1.pack(padx=5, pady=5)
+    label1.grid(row=0, column=0)
     entry1 = tk.Entry(creatureOneFrame)
-    entry1.pack(pady=3)
+    entry1.grid(row=0, column=1)
 
     # Create the second frame for the second creature.
     creatureTwoFrame = tk.Frame(root)
-    creatureTwoFrame.pack(side="top", anchor="w", padx=30, pady=5)
+    creatureTwoFrame.grid(row=2, column=0, sticky="w", padx=10)
     # Populate the second frame with a label and an entry.
     label2 = tk.Label(creatureTwoFrame, text="Creature 2:")
-    label2.pack(padx=5, pady=5)
+    label2.grid(row=0, column=0)
     entry2 = tk.Entry(creatureTwoFrame)
-    entry2.pack(pady=3)
+    entry2.grid(row=0, column=1)
 
     # Create an empty "go" button.
-    goButton = tk.Button(creatureTwoFrame, text="Go!", width=10, command=lambda: print("Button clicked!"))
-    goButton.pack(side="top", padx=10, pady=20)
+    goButton = tk.Button(root, text="Go!", width=25, height=4, command=lambda: print("Button clicked!"))
+    goButton.grid(row=3, column=0, pady=15, padx=30)
+
+    # Create an empty square frame with a gray background.
+    outputFrame = tk.Frame(root, bg="grey", width=250, height=200)
+    outputFrame.grid(row=1, column=1, rowspan=3, sticky="nsew", padx=25)
+    outputFrame.grid_propagate(False)  # Prevent the frame from resizing to fit its contents.
 
 
-# Call and create the main window
 def startGui():
     root = tk.Tk()
     root.title("Creature Creator")
